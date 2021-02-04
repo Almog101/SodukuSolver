2cdad0ac4e16f432f21a13bb825fea9c2d7c24c5
Almog101 <almogba8@gmail.com>
2021-02-04 21:12:44 +0200

    commit

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from soduku import Solve, toHex
 
 # better tkinter entry with max length and digit input mask
 class NewEntry(tk.Entry):
-    def __init__(self, master=None, max_len=5, **kwargs):
+    def __init__(self, master=None, font_size = 0, width = 0, max_len=0, **kwargs):
         self.var = tk.StringVar()
         self.max_len = max_len
-        tk.Entry.__init__(self, master, textvariable=self.var,width=3, font=("default", 35), justify='center', **kwargs)
+        tk.Entry.__init__(self, master, textvariable=self.var,width=width, font=("default", font_size), justify='center', **kwargs)
         self.old_value = ''
         self.var.trace('w', self.check)
 
@@ -48,20 +48,20 @@ window.geometry('740x750')
 
 
 #generate the board (entries)
-TextBoxes = []
+entries = []
 for row in range(9):
     for column in range(9):
-        entry = NewEntry(window, max_len=1)
-        entry.grid(column=column, row=row, ipady=10)
-        TextBoxes.append(entry)
-    
+        entry = NewEntry(window, max_len=1, width=3, font_size=35)
+        entry.grid(column=int(column*(1+1/3)), row=int(row*(1+1/3)), ipady=10)
+        entries.append(entry)
+
 # initialize the solve button
-solveBtn = tk.Button(window, text="Solve Board", command= lambda: solveBoard(entries=TextBoxes,window=window), height=3)
-solveBtn.grid(column=4, row=9)
+solveBtn = tk.Button(window, text="Solve Board", command= lambda: solveBoard(entries=entries,window=window), height=3)
+solveBtn.grid(column=4, row=12)
 
 # initialize the clear button
-clearBtn = tk.Button(window, text="Clear Board", command= lambda: clear(TextBoxes), height=3)
-clearBtn.grid(column=5, row=9)
+clearBtn = tk.Button(window, text="Clear Board", command= lambda: clear(entries), height=3)
+clearBtn.grid(column=6, row=12)
 
 # tkinter window loop
 window.mainloop()
